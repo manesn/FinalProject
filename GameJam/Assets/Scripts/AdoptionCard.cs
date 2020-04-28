4d285e8a8fcdd5a5417c32259ac70138f470773d
Jocelyn <joeads@iu.edu>
2020-04-28 01:32:31 -0400

    feat: SO for chars
    Made scripts for the SOs that update with correct information

---
+++ GameJam/Assets/Scripts/AdoptionCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Adoption Card", menuName = "Adoption Card")]
+public class AdoptionCard : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+}
---
+++ GameJam/Assets/Scripts/AdoptionCardDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdoptionCardDisplay : MonoBehaviour
+{
+    public AdoptionCard adoptionCard;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Button adoptButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = adoptionCard.name;
+        descriptionText.text = adoptionCard.description;
+
+        artworkImage.sprite = adoptionCard.artwork;
+
+    }
+
+   
+}
